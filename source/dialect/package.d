eb3d4e3d564ac93bdc3dd997acb3a4e0266a1cf4
JR <zorael@gmail.com>
2019-09-05 01:24:58 +0200

    Initial code dump of the IRC parsing library modules

---
+++ source/dialect/package.d
@@ -0,0 +1,9 @@
+/++
+ +  This is a package file for all the IRC modules. It only publicly imports
+ +  them and does not contain any code itself.
+ +/
+module dialect;
+
+public import dialect.defs;
+public import dialect.parsing;
+public import dialect.common;
