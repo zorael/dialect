67a056b06bcf5f26132d7f73d8eca96e62ad49c3
JR <zorael@gmail.com>
2020-06-10 18:46:11 +0200

    v0.6.1
    This is a patch release mostly adding definitions for the new Twitch
    "client-nonce" tag. The nonexistence of which causes warnings in
    importing projects.
    
    Other changes:
    * Removed TWITCH_SKIPSUBSMODEMESSAGE, which should have been removed in
      v0.6.0. Its current presence is a bug.
    * Some in-contracts

--- source/dialect/semver.d
+++ source/dialect/semver.d
@@ -12,7 +12,7 @@ enum DialectSemVer
 {
     majorVersion = 0,  /// SemVer major version of the library.
     minorVersion = 6,  /// SemVer minor version of the library.
-    patchVersion = 0,  /// SemVer patch version of the library.
+    patchVersion = 1,  /// SemVer patch version of the library.
 }
 
 
