7c28ea962ad31cdfc8809935b4114b134d93b831
JR <zorael@gmail.com>
2020-10-01 00:06:33 +0200

    ddoc vs comment

--- source/dialect/common.d
+++ source/dialect/common.d
@@ -515,9 +515,9 @@ do
 {
     import std.string : representation;
 
-    /++
+    /+
         Channels names are strings (beginning with a '&' or '#' character) of
-        length up to 200 characters.  Apart from the the requirement that the
+        length up to 200 characters.  Apart from the requirement that the
         first character being either '&' or '#'; the only restriction on a
         channel name is that it may not contain any spaces (' '), a control G
         (^G or ASCII 7), or a comma (',' which is used as a list item
