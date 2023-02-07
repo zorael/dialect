8ef092dbed4c79d41f3b2646c458e48f861ba6a0
JR <zorael@gmail.com>
2023-02-07 14:21:45 +0100

    v2.0.0
    This release introduces some breaking changes and thus (this time
    faithfully) bumps the major version.
    
    Build configurations were reworked and are now as follows;
    
    * `library` is the base configuration
    * `twitch` includes extra parsing needed to interface with Twitch
      servers
    * `bot` includes some code specifically useful for bot applications
    * `twitchbot` is `twitch` and `bot` combined
    
    Importing projects may need to update their `dub.sdl`/`dub.json` files
    to ensure the wanted functionality is compiled.
    
    `IRCEvent.aux` and `IRCEvent.{count,altcount}` were changed from being a
    single string and two longs to instead be a `string[16]` and a
    `Nullable!long[16]` respectively. This extra real estate allows us to
    retain some information that was previously discarded, or in other cases
    were flattened and concatenated into one string. Consequently, parsing
    of some event types was changed to preserve this information.
    
    For *most* types of events, *generally* only one `aux` string is ever
    required, and *typically* two `count` longs are enough. As such,
    importing projects *largely* only need to change `aux` to `aux[0]`,
    `count` to `count[0]` and `altcount` to `count[1]` -- except where they
    don't. The parsing of `RPL_ISUPPORT` and `CAP` events are examples of
    where this is insufficient, and manual conversions must be made.
    
    * `RPL_ISUPPORT`; individual supports are now stored in array `aux`
    * `CAP`; individual capabilities are now stored in array `aux` and the
      verb (e.g. "`LS`") is in `content`
    
    Many Twitch events that carry numerical counts now store it in `count`
    as an array. Refer to the implementation for specifics (use the source,
    Luke!).
    
    Twitch sunsetted its stream host mode some time back, and so the
    relevant types (`HOSTTARGET`, `TWITCH_HOSTSTART` and `TWITCH_HOSTEND`)
    and the parsing for them were finally removed.
    
    Postprocessors are no longer instantiated by having the string form of
    their parent modules declared in an `AliasSeq` in
    `postprocessors/package.d`, but instead are registered by mixing in
    `PostprocessorRegistration!MyPostprocessor` at the module scope. The
    original solution worked, but was ultimately inelegant and could run
    into difficulties with module constructors ending up with cyclic
    dependencies.
    
    Importing projects need to make sure to import postprocessor modules in
    at least one place in their source! Without this, the Postprocessor
    module's constructors are not run and the Postprocessor class never gets
    instantiated. This may change in the future.

--- source/dialect/semver.d
+++ source/dialect/semver.d
@@ -10,8 +10,8 @@ module dialect.semver;
 /// SemVer versioning of this build.
 enum DialectSemVer
 {
-    major = 1,  /// SemVer major version of the library.
-    minor = 5,  /// SemVer minor version of the library.
+    major = 2,  /// SemVer major version of the library.
+    minor = 0,  /// SemVer minor version of the library.
     patch = 0,  /// SemVer patch version of the library.
 }
 
