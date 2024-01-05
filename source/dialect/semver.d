6f56693d1df46a4c7ef5ff0d3409bbc528c22cf8
JR <zorael@gmail.com>
2024-01-05 11:56:05 +0100

    v2.1.0
    This is a minor release that updates the `lu` dependency to v2.1.0.
    
    * Added Twitch event type `TWITCH_MILESTONE`
    * Various fixes and improvements

--- source/dialect/semver.d
+++ source/dialect/semver.d
@@ -11,8 +11,8 @@ module dialect.semver;
 enum DialectSemVer
 {
     major = 2,  /// SemVer major version of the library.
-    minor = 0,  /// SemVer minor version of the library.
-    patch = 4,  /// SemVer patch version of the library.
+    minor = 1,  /// SemVer minor version of the library.
+    patch = 0,  /// SemVer patch version of the library.
 }
 
 
